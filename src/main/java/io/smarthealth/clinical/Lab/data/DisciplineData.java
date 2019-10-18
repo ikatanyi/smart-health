93935a10c19368060721d1f9fcacb57dd6f247c3
Simon Waweru <simon.waweru@smartapplicationsgroup.com>
2019-10-18 12:30:36 +0300

    Sync lab module with dev

---
+++ src/main/java/io/smarthealth/clinical/Lab/data/DisciplineData.java
@@ -0,0 +1,13 @@
+package io.smarthealth.clinical.lab.data;
+
+import lombok.Data;
+
+/**
+ *
+ * @author Kennedy.Imbenzi
+ */
+@Data
+public class DisciplineData  {
+    private Long id;
+    private String discipline;
+}
