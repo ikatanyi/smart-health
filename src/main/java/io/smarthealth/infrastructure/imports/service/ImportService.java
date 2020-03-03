dbfa28054fb7b8faf8cdd4ddf2dec23c8145354a
kent <kent@10.191.171.162>
2020-03-03 08:18:21 +0300

    -fixed a bug in jsperReports service -Added Templating service

---
+++ src/main/java/io/smarthealth/infrastructure/imports/service/ImportService.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.smarthealth.infrastructure.imports.service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Kennedy.Imbenzi
+ */
+@Service
+public class ImportService {
+    public void exportExcel(String name, String fileName, Map<Integer, List> data, HttpServletResponse response) throws IOException {
+
+        //Blank workbook
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        //Create a blank sheet
+        XSSFSheet sheet = workbook.createSheet(name);
+
+        CellStyle cellStyle = workbook.createCellStyle();
+        CreationHelper createHelper = workbook.getCreationHelper();
+        cellStyle.setDataFormat(createHelper.createDataFormat().getFormat("yyyy-MM-dd"));
+
+        // Create a Font for styling header cells
+        XSSFFont headerFont = workbook.createFont();
+        headerFont.setBold(true);
+        headerFont.setFontHeightInPoints((short) 12);
+//        headerFont.setColor(IndexedColors.RED.getIndex());
+
+        // Create a CellStyle with the font
+        CellStyle headerCellStyle = workbook.createCellStyle();
+        headerCellStyle.setFont(headerFont);
+
+        ServletOutputStream servletOutputStream = response.getOutputStream();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        //Iterate over data and write to sheet
+        Set<Integer> keyset = data.keySet();
+        int rownum = 0;
+        for (int key : keyset) {
+            Row row = sheet.createRow(rownum++);
+            List objArr = data.get(key);
+            int cellnum = 0;
+            for (Object obj : objArr) {
+                Cell cell = row.createCell(cellnum++);
+
+                // Create cells
+                cell.setCellValue((String) obj);
+                cell.setCellStyle(headerCellStyle);
+            }
+        }
+        try {
+            //Write the workbook in file system
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("Content-Disposition", "attachment; filename=" + fileName + ".xls");
+            workbook.write(response.getOutputStream());
+            workbook.close();
+
+            System.out.println();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/io/smarthealth/report/domain/Header.java
+++ src/main/java/io/smarthealth/report/domain/Header.java
@@ -14,17 +14,18 @@ import lombok.Data;
  */
 @Data
 public class Header {
+
     private String facilityName;
     private String facilityType;
-    private byte[] logo;    
-    private String orgLegalName; 
-    private String orgName; 
+    private byte[] logo;
+    private String orgLegalName;
+    private String orgName;
     private String taxNumber;
     private String orgWebsite;
     private String orgAddressCountry;
     private String orgAddressCounty;
     private String orgAddressLine1;
-    private String orgAddressLine2; 
+    private String orgAddressLine2;
     private String orgPostalCode;
     private String orgTown;
     private String orgType;
@@ -33,37 +34,38 @@ public class Header {
     private String contactMobile;
     private String salutation;
     private String telephone;
-    
-    
-    public static Header map(Facility facility){
+
+    public static Header map(Facility facility) {
         Header header = new Header();
-        if (!facility.getOrganization().getContact().isEmpty()) {
-            header.setContactEmail(facility.getOrganization().getContact().get(0).getEmail());
-            header.setContactFullName(facility.getOrganization().getContact().get(0).getFullName());
-            header.setContactMobile(facility.getOrganization().getContact().get(0).getMobile());
-            header.setSalutation(facility.getOrganization().getContact().get(0).getSalutation());
-            header.setTelephone(facility.getOrganization().getContact().get(0).getTelephone());
+        if (facility.getOrganization() != null) {
+            if (!facility.getOrganization().getContact().isEmpty()) {
+                header.setContactEmail(facility.getOrganization().getContact().get(0).getEmail());
+                header.setContactFullName(facility.getOrganization().getContact().get(0).getFullName());
+                header.setContactMobile(facility.getOrganization().getContact().get(0).getMobile());
+                header.setSalutation(facility.getOrganization().getContact().get(0).getSalutation());
+                header.setTelephone(facility.getOrganization().getContact().get(0).getTelephone());
+                header.setSalutation(facility.getOrganization().getContact().get(0).getSalutation());
+                header.setTelephone(facility.getOrganization().getContact().get(0).getTelephone());
+            }
+
+            if (facility.getOrganization().getAddress()!=null && !facility.getOrganization().getAddress().isEmpty()) {
+                header.setOrgAddressCountry(facility.getOrganization().getAddress().get(0).getCountry());
+                header.setOrgAddressCounty(facility.getOrganization().getAddress().get(0).getCounty());
+                header.setOrgAddressLine1(facility.getOrganization().getAddress().get(0).getLine1());
+                header.setOrgAddressLine2(facility.getOrganization().getAddress().get(0).getLine2());
+                header.setOrgPostalCode(facility.getOrganization().getAddress().get(0).getPostalCode());
+                header.setOrgTown(facility.getOrganization().getAddress().get(0).getTown());
+                header.setOrgType(facility.getOrganization().getAddress().get(0).getType().name());
+            }
+            header.setOrgLegalName(facility.getOrganization().getLegalName());
+            header.setOrgName(facility.getOrganization().getOrganizationName());
+            header.setOrgWebsite(facility.getOrganization().getWebsite());
+            header.setTaxNumber(facility.getOrganization().getTaxNumber());
+            header.setFacilityName(facility.getFacilityName());
+            header.setFacilityType(facility.getFacilityType());
+            header.setLogo(facility.getLogo());
         }
-        header.setFacilityName(facility.getFacilityName());
-        header.setFacilityType(facility.getFacilityType());
-        header.setLogo(facility.getLogo());
-        header.setOrgAddressCountry(facility.getOrganization().getAddress().get(0).getCountry());
-        header.setOrgAddressCounty(facility.getOrganization().getAddress().get(0).getCounty());
-        header.setOrgAddressLine1(facility.getOrganization().getAddress().get(0).getLine1());
-        header.setOrgAddressLine2(facility.getOrganization().getAddress().get(0).getLine2());
-        header.setOrgLegalName(facility.getOrganization().getLegalName());
-        header.setOrgName(facility.getOrganization().getOrganizationName());
-        header.setOrgWebsite(facility.getOrganization().getWebsite());
-        header.setTaxNumber(facility.getOrganization().getTaxNumber());
-        if (!facility.getOrganization().getAddress().isEmpty()) {
-            header.setOrgPostalCode(facility.getOrganization().getAddress().get(0).getPostalCode());
-            header.setOrgTown(facility.getOrganization().getAddress().get(0).getTown());
-            header.setOrgType(facility.getOrganization().getAddress().get(0).getType().name());            
-            header.setSalutation(facility.getOrganization().getContact().get(0).getSalutation());            
-            header.setTelephone(facility.getOrganization().getContact().get(0).getTelephone());
-            
-        }
-        
+
         return header;
     }
 }
