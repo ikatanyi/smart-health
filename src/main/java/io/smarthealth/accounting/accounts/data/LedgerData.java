054b8e5041c081937b4a6de4336c4950ee52a9d0
Kelsas <kelvin.sasaka@smartapplicationsgroup.com>
2020-06-09 19:17:28 +0300

    generating getters and setters for Ledger to avoid the duplicate lombok warnings

--- src/main/java/io/smarthealth/accounting/accounts/data/LedgerData.java
+++ src/main/java/io/smarthealth/accounting/accounts/data/LedgerData.java
@@ -8,7 +8,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;  
 import lombok.Data;
-  
+ 
 @Data
 public final class LedgerData {
 
