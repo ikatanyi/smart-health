ddbc7662d174e654ba6b40e1ab83d5c1509da71b
Simon.waweru <Simon.waweru@LP-TECH-4Z0Y.smart.local>
2020-07-09 22:58:08 +0300

    ivory clinical data sql injector

---
+++ src/main/java/io/smarthealth/infrastructure/utility/ivorydata/DBConnector.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.smarthealth.infrastructure.utility.ivorydata;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Simon.waweru
+ */
+public class DBConnector {
+
+    String pastDB = "jdbc:mysql://127.0.0.1:3306/dbo?useSSL=false&allowPublicKeyRetrieval=true&zeroDateTimeBehavior = CONVERT_TO_NULL";
+
+    String currentDB = "jdbc:mysql://127.0.0.1:3306/smarthealth?useSSL=false&allowPublicKeyRetrieval=true&zeroDateTimeBehavior = CONVERT_TO_NULL";
+
+    private final String USERNM = "smarthealth";
+    private final String PASSWD = "Sm@rt_123";
+
+    public Connection ConnectToPastDB() throws Exception {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connMain = DriverManager.getConnection(pastDB, USERNM, PASSWD);
+            return connMain;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            throw new Exception("Error connection ", e);
+        }
+    }
+}
