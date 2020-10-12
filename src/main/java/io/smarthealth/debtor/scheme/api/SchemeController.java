266abb56059a282df86376179af2f2280808b6b6
Kelsas <kelvin.sasaka@smartapplicationsgroup.com>
2020-10-12 08:52:53 +0300

    Merge origin/dev into dev
    Conflicts:
    	src/main/resources/application.properties

--- src/main/java/io/smarthealth/debtor/scheme/api/SchemeController.java
+++ src/main/java/io/smarthealth/debtor/scheme/api/SchemeController.java
@@ -201,19 +201,23 @@ public class SchemeController {
             //look for scheme config 
             SchemeConfigurations schemeConfig = schemeService.fetchSchemeConfigById(data.getConfigId());
             schemeConfig.setCoPayType(data.getCoPayType());
+            schemeConfig.setCopayEnabled(data.isCopayEnabled());
             schemeConfig.setCoPayValue(data.getCoPayValue());
             schemeConfig.setDiscountMethod(data.getDiscountMethod());
             schemeConfig.setDiscountValue(data.getDiscountValue());
 //            schemeConfig.setScheme(scheme);
             schemeConfig.setSmartEnabled(data.isSmartEnabled());
             schemeConfig.setStatus(data.isStatus());
+            schemeConfig.setCapitationAmount(data.getCapitationAmount());
+            schemeConfig.setCapitationEnabled(data.isCapitationEnabled());
             configSaved = schemeService.updateSchemeConfigurations(schemeConfig);
         }
 
-        Pager<SchemConfigData> pagers = new Pager();
+        Scheme myscheme=configSaved.getScheme();
+        Pager<SchemeData> pagers = new Pager();
         pagers.setCode("0");
         pagers.setMessage("Scheme parameters have successfully been updated");
-        pagers.setContent(SchemConfigData.map(configSaved));
+        pagers.setContent(SchemeData.map(myscheme));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(pagers);
 
