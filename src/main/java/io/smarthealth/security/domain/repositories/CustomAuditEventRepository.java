e1dc2ddd6a3dfe36c4bbc6fd81d7845a989cf74c
Kelsas <kelvin.sasaka@smartapplicationsgroup.com>
2020-10-21 17:47:35 +0300

    Merging dev

---
+++ src/main/java/io/smarthealth/security/domain/repositories/CustomAuditEventRepository.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.smarthealth.security.domain.repositories;
+
+import io.smarthealth.config.Constants;
+import io.smarthealth.security.config.audit.AuditEventConverter;
+import io.smarthealth.security.domain.PersistentAuditEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.audit.AuditEvent;
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.*;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * An implementation of Spring Boot's {@link AuditEventRepository}.
+ */
+@Slf4j
+@Repository
+public class CustomAuditEventRepository implements AuditEventRepository {
+
+    private static final String AUTHORIZATION_FAILURE = "AUTHORIZATION_FAILURE";
+
+    /**
+     * Should be the same as in Liquibase migration.
+     */
+    protected static final int EVENT_DATA_COLUMN_MAX_LENGTH = 100000;
+
+    private final PersistenceAuditEventRepository persistenceAuditEventRepository;
+
+    private final AuditEventConverter auditEventConverter;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    public CustomAuditEventRepository(PersistenceAuditEventRepository persistenceAuditEventRepository,
+            AuditEventConverter auditEventConverter) {
+
+        this.persistenceAuditEventRepository = persistenceAuditEventRepository;
+        this.auditEventConverter = auditEventConverter;
+    }
+
+    @Override
+    public List<AuditEvent> find(String principal, Instant after, String type) {
+        Iterable<PersistentAuditEvent> persistentAuditEvents =
+            persistenceAuditEventRepository.findByPrincipalAndAuditEventDateAfterAndAuditEventType(principal, after, type);
+        return auditEventConverter.convertToAuditEvent(persistentAuditEvents);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void add(AuditEvent event) {
+//        log.info(event.toString());
+        if (!AUTHORIZATION_FAILURE.equals(event.getType()) &&
+            !Constants.ANONYMOUS_USER.equals(event.getPrincipal())) {
+
+            PersistentAuditEvent persistentAuditEvent = new PersistentAuditEvent();
+            persistentAuditEvent.setPrincipal(event.getPrincipal());
+            persistentAuditEvent.setAuditEventType(event.getType());
+            persistentAuditEvent.setAuditEventDate(event.getTimestamp());
+            Map<String, String> eventData = auditEventConverter.convertDataToStrings(event.getData());
+            persistentAuditEvent.setData(truncate(eventData));
+            persistenceAuditEventRepository.save(persistentAuditEvent);
+        }
+    }
+
+    /**
+     * Truncate event data that might exceed column length.
+     */
+    private Map<String, String> truncate(Map<String, String> data) {
+        Map<String, String> results = new HashMap<>();
+
+        if (data != null) {
+            for (Map.Entry<String, String> entry : data.entrySet()) {
+                String value = entry.getValue();
+                if (value != null) {
+                    int length = value.length();
+                    if (length > EVENT_DATA_COLUMN_MAX_LENGTH) {
+                        value = value.substring(0, EVENT_DATA_COLUMN_MAX_LENGTH);
+                        log.warn("Event data for {} too long ({}) has been truncated to {}. Consider increasing column width.",
+                                 entry.getKey(), length, EVENT_DATA_COLUMN_MAX_LENGTH);
+                    }
+                }
+                results.put(entry.getKey(), value);
+            }
+        }
+        return results;
+    }
+}
