1089a1436a8de0ec1b872d7d16d80834a706caa5
Kelsas <kelvin.sasaka@smartapplicationsgroup.com>
2020-11-06 15:52:15 +0300

    * Journal reversal * Receipt cancellation/voiding completely

---
+++ src/main/java/io/smarthealth/organization/person/patient/domain/impl/CustomizedPatientRepositoryImpl.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.smarthealth.organization.person.patient.domain.impl;
+
+import io.smarthealth.organization.person.patient.domain.CustomizedPatientRepository;
+import io.smarthealth.organization.person.patient.domain.Patient;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
+
+/**
+ *
+ * @author Kelsas
+ */
+public class CustomizedPatientRepositoryImpl implements CustomizedPatientRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+//hibernate-search lucene 
+    @Override
+    public List<Patient> search(String terms, int limit, int offset) {
+        FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(em);
+        QueryBuilder queryBuilder = fullTextEntityManager.getSearchFactory()
+                .buildQueryBuilder().forEntity(Patient.class).get();
+
+        org.apache.lucene.search.Query luceneQuery = queryBuilder
+                .keyword()
+                .onFields("givenName", "middleName", "surname", "patientNumber", "primary_contact")
+                .matching(terms)
+                .createQuery(); 
+
+        // wrap Lucene query in a javax.persistence.Query
+        javax.persistence.Query jpaQuery
+                = fullTextEntityManager.createFullTextQuery(luceneQuery, Patient.class);
+        jpaQuery.setMaxResults(limit);
+        jpaQuery.setFirstResult(offset);
+        // execute search
+        return jpaQuery.getResultList();
+    }
+
+}
