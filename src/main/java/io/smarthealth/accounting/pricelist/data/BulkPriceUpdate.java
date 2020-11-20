b9f1076ae5fc8807b4ffe41ca6a1c1afe369688f
Kelsas <kelvin.sasaka@smartapplicationsgroup.com>
2020-11-20 20:27:48 +0300

    * Adding quantity returned on pharmacy return * Batch pricebook update of an Item endpoint * Journal fix on Stock opening balance import

---
+++ src/main/java/io/smarthealth/accounting/pricelist/data/BulkPriceUpdate.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.smarthealth.accounting.pricelist.data;
+
+import java.math.BigDecimal;
+import lombok.Data;
+
+/**
+ *
+ * @author Kelsas
+ */
+@Data
+public class BulkPriceUpdate {
+
+    private Long itemId;
+    private String itemCode;
+    private String itemName;
+    private BigDecimal amount;
+    private int[] pricebooks;
+}
